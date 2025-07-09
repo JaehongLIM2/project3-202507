39c77e1bbf4ca0fb25d41389cb660ea7e7dfc0e6
JaehongLIM2 <nu0044@naver.com>
2025-07-09 12:26:29 +0900

    게시물 테이블 출력

--- backend/src/main/java/com/example/backend/board/controller/BoardController.java
+++ backend/src/main/java/com/example/backend/board/controller/BoardController.java
@@ -3,24 +3,32 @@ package com.example.backend.board.controller;
 import com.example.backend.board.dto.BoardDto;
 import com.example.backend.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
-@Controller
+//@Controller
+//@ResponseBody
+@Slf4j
+@RestController // Controller + ResponseBody
 @RequestMapping("/api/board")
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
 
+    @GetMapping("list")
+    public ResponseEntity<List<BoardDto>> getAllBoards() {
+        System.out.println("BoardController.getAllBoards");
+        return null;
+    }
+
+
     @PostMapping("add")
-    @ResponseBody
     public ResponseEntity<Object> add(@RequestBody BoardDto dto) {
 
         // 값들이 유효한지 확인
--- frontend/src/feature/board/BoardList.jsx
+++ frontend/src/feature/board/BoardList.jsx
@@ -1,7 +1,61 @@
+import { Col, Row, Spinner, Table } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export function BoardList() {
+  const [boardList, setBoardList] = useState(null);
+
+  useEffect(() => {
+    // 마운트 될 때 (initial render 시) 실행되는 코드
+    axios
+      .get("/api/board/list")
+      .then((res) => {
+        console.log("잘 될 때 코드");
+        setBoardList(res.data);
+      })
+      .catch((err) => {
+        console.log("잘 안될 때 코드");
+      })
+      .finally(() => {
+        console.log("항상 실행되는 코드ㄸ");
+      });
+  }, []);
+
+  if (!boardList) {
+    return <Spinner />;
+  }
+
   return (
-    <div>
-      <h3>글 목록</h3>
-    </div>
+    <Row>
+      <Col>
+        <h2 className="mb-4">글 목록</h2>
+        {boardList.length > 0 ? (
+          <Table striped={true} hover={true}>
+            <thead>
+              <th>
+                <td>#</td>
+                <td>제목</td>
+                <td>작성자</td>
+                <td>작성일시</td>
+              </th>
+            </thead>
+            <tbody>
+              {boardList.map((board) => (
+                <tr key={board.id}>
+                  <td>{board.id}</td>
+                  <td>{board.title}</td>
+                  <td>{board.author}</td>
+                  <td>{board.insertedAt}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        ) : (
+          <p>
+            작성된 글이 없습니다. <br />새 글을 작성해 보세요.
+          </p>
+        )}
+      </Col>
+    </Row>
   );
 }
