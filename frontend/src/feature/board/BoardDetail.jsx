7abf56f833b4c257c56c1ed84750cbeb5d3b0ac1
JaehongLIM2 <nu0044@naver.com>
2025-07-09 14:34:16 +0900

    게시물 보기

--- frontend/src/feature/board/BoardDetail.jsx
+++ frontend/src/feature/board/BoardDetail.jsx
@@ -1,9 +1,19 @@
 import { useParams } from "react-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import {
+  Button,
+  Col,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  Row,
+  Spinner,
+} from "react-bootstrap";
 
 export function BoardDetail() {
+  const [board, setBoard] = useState(null);
   const { id } = useParams();
   useEffect(() => {
     // axios 로 해당 게시물 가져오기
@@ -11,6 +21,7 @@ export function BoardDetail() {
       .get(`/api/board/${id}`)
       .then((res) => {
         console.log("잘됨");
+        setBoard(res.data);
       })
       .catch((err) => {
         console.log("안됨");
@@ -21,5 +32,55 @@ export function BoardDetail() {
       });
   }, []);
 
-  return <h1>게시물 보기</h1>;
+  // 만약 게시물이 없으면 스피너
+  if (!board) {
+    return <Spinner />;
+  }
+
+  return (
+    <Row className="justify-content-center">
+      <Col xs={12} md={8} lg={6}>
+        <h3 className="mb-4">{board.id}번 게시물</h3>
+        <div>
+          <FormGroup className="mb-3" controlId="title1">
+            <FormLabel>제목</FormLabel>
+            <FormControl readOnly={true} value={board.title} />
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup className="mb-3" controlId="content1">
+            <FormLabel>본문</FormLabel>
+            <FormControl
+              as="textarea"
+              row={6}
+              readOnly={true}
+              value={board.content}
+            />
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup className="mb-3" controlId="author1">
+            <FormLabel>작성자</FormLabel>
+            <FormControl readOnly={true} value={board.author} />
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup className="mb-3" controlId="insertedAt1">
+            <FormLabel>작성일시</FormLabel>
+            <FormControl
+              type="datetime-local"
+              readOnly={true}
+              value={board.insertedAt}
+            />
+          </FormGroup>
+        </div>
+        <div>
+          <Button className="me-2" variant="outline-danger">
+            삭제
+          </Button>
+          <Button variant="outline-info">수정</Button>
+        </div>
+      </Col>
+    </Row>
+  );
 }
