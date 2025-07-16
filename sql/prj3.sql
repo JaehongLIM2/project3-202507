4ae26772a7f063895f61c9c5e48ebeb240b3b666
JaehongLIM2 <nu0044@naver.com>
2025-07-16 09:43:33 +0900

    하트 클릭시 좋아요 입력 삭제

--- sql/prj3.sql
+++ sql/prj3.sql
@@ -75,7 +75,14 @@ CREATE TABLE comment
     FOREIGN KEY (board_id) REFERENCES board (id)
 );
 
-
+CREATE TABLE board_like
+(
+    board_id     INT          NOT NULL,
+    member_email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (board_id, member_email),
+    FOREIGN KEY (board_id) REFERENCES board (id),
+    FOREIGN KEY (member_email) REFERENCES member (email)
+);
 
 
 
